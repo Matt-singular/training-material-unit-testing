e0924e9bfb946eede36e6dea458a2b7e495e664f
Matthew Lewis <135806496+Matt-singular@users.noreply.github.com>
2025-02-10 09:14:25 +0200

    Added the Application project

---
+++ Application/Program.cs
@@ -0,0 +1,15 @@
+﻿using Application;
+using Microsoft.Extensions.DependencyInjection;
+
+// Dependency Injection
+var serviceProvider = new ServiceCollection()
+  .AddDomainServices()
+  .BuildServiceProvider();
+
+// Execute Example
+//var calculator = serviceProvider.GetRequiredService<ICalculator>();
+//var result = calculator.Add(1, 2);
+
+// Write the results and keep the terminal open
+Console.WriteLine(string.Empty);
+Console.ReadLine();
---
+++ Application/StartupExtensions.cs
@@ -0,0 +1,19 @@
+﻿namespace Application;
+
+using Microsoft.Extensions.DependencyInjection;
+
+/// <summary>
+/// Contains extension methods for the <see cref="Program"/> startup class.
+/// </summary>
+public static class StartupExtensions
+{
+  /// <summary>
+  /// Adds the domain services to the service collection.
+  /// </summary>
+  /// <param name="services">The service collection.</param>
+  /// <returns>The service collection with the registered domain services.</returns>
+  public static IServiceCollection AddDomainServices(this IServiceCollection services)
+  {
+    return services;
+  }
+}
