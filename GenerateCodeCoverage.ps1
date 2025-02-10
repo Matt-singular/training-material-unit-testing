a4a97cc51f7790e6490ccb5abb2b5e803b93e0de
Matthew Lewis <135806496+Matt-singular@users.noreply.github.com>
2025-02-10 20:24:00 +0200

    Updated the GenerateCodeCoverage script

--- GenerateCodeCoverage.ps1
+++ GenerateCodeCoverage.ps1
@@ -1,13 +1,28 @@
 ### This script will run dotnet tests and then generate a code coverage report from the results
 ### If you receive a digital signing error then run the follwoing command: Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope Process
 
-# Clear out the contents of the TestResults folder
-if (Test-Path -Path "Domain.Tests/TestResults") {
-    Get-ChildItem -Path "Domain.Tests/TestResults" | Remove-Item -Recurse -Force
+# 1. Clear out the contents of the TestResults folder
+function Clear-TestResultsFolder 
+{
+    param ([string]$path)
+    if (Test-Path -Path $path) {
+        Get-ChildItem -Path $path | Remove-Item -Recurse -Force
+    }
 }
 
-# Generate the code coverage using Coverlet
+Clear-TestResultsFolder -path "Domain.Tests/TestResults"
+Clear-TestResultsFolder -path "IntegrationTests/TestResults"
+
+
+# 2. Generate the code coverage using Coverlet
 dotnet test --collect:"XPlat Code Coverage"
 
-# Generate a CoverageReport
-reportgenerator -reports:Domain.Tests/TestResults/*/coverage.cobertura.xml -targetdir:Application/CoverageReport
+# 3. Generate a CoverageReport
+$reportPaths = @(
+    "Domain.Tests/TestResults/*/coverage.cobertura.xml",
+    "IntegrationTests/TestResults/*/coverage.cobertura.xml"
+)
+
+$reportPathsString = [string]::Join(";", $reportPaths)
+
+reportgenerator -reports:$reportPathsString -targetdir:Application/CoverageReport
