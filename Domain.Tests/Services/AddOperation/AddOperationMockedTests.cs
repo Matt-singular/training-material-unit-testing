e36e2155b09edfd1a746617df7ebb8d44b23eece
Matthew Lewis <135806496+Matt-singular@users.noreply.github.com>
2025-02-10 14:18:21 +0200

    Added a very basic mock test

---
+++ Domain.Tests/Services/AddOperation/AddOperationMockedTests.cs
@@ -0,0 +1,23 @@
+﻿namespace Domain.Tests.Services.AddOperation;
+
+using Domain.Services;
+
+public partial class AddOperationTests
+{
+  [Fact]
+  public void Execute_AddsNumbers_UsingMockedService()
+  {
+    // Arrange
+    var mockedAddOperationService = Substitute.For<ICalculatorOperation>();
+
+    mockedAddOperationService
+      .Execute(Arg.Any<decimal?>(), Arg.Any<decimal?>())
+      .Returns(decimal.One);
+
+    // Act
+    decimal result = mockedAddOperationService.Execute(1, 0);
+
+    // Assert
+    Assert.Equal(1, result);
+  }
+}
